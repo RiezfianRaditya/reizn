5740342e0847dc2f7d8bcf53f80621c3c7b4d5fa
RiezfianRaditya <riezfianraditya@gmail.com>
2021-10-14 17:05:20 +0800

    reizn

---
+++ app/Http/Controllers/API/TransactionController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class TransactionController extends Controller
+{
+    public function all(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit', 6);
+        $status = $request->input('status');
+
+        if ($id) {
+            $transaction = Transaction::with(['item.product'])->find($id);
+
+            if ($transaction) {
+                ResponseFormatter::success(
+                    $transaction,
+                    'Data Transaksi Berhasil Diambil'
+                );
+            } else {
+                ResponseFormatter::error(
+                    null,
+                    'Data Transaksi Tidak ada',
+                    404
+                );
+            }
+        }
+
+        $transaction = Transaction::with(['items.product'])->where('users_id', Auth::user()->id);
+
+        if ($status) {
+            $transaction->where('status', $status);
+        }
+
+        return ResponseFormatter::success(
+            $transaction->paginate($limit),
+            'Data list transaksi berhasl diambil'
+        );
+    }
+
+    public function checkout(Request $request)
+    {
+        Validator::make($request->all(), [
+            'items' => 'required|array',
+            'items.*.id' => 'exists:products,id',
+            'total_price' => 'required',
+            'shipping_price' => 'required',
+            'status' => 'required|in:PENDING,SUCCESS,CANCELED,FAILED,SHIPPED,SHIPPING'
+        ]);
+
+        $transaction = Transaction::create([
+            'users_id' => Auth::user()->id,
+            'address' => $request->address,
+            'total_price' => $request->total_price,
+            'shipping_price' => $request->shipping_price,
+            'status' => $request->status,
+        ]);
+
+        foreach ($request->items as $product) {
+            Transaction::create([
+                'users_id' => Auth::user()->id,
+                'products_id' => $product['id'],
+                'transactions_id' => $transaction->id,
+                'quantity' => $product['quantity'],
+            ]);
+        }
+
+        return ResponseFormatter::success($transaction->load('items.product'), 'Transaksi Berhasil');
+    }
+}
